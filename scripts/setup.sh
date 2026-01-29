8f68affb5e56453f8d478f59e400be28ec8764d8
Paul Marshall <marshalp@uchicago.edu>
2026-01-29 10:08:32 -0600

    Update the notebook and scripts for external artifact setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 
-echo "Setting up experimental environment"
+echo "Setting up artifact environment"
 
 set -x
 
 sudo apt-get update -yqq
-sudo apt-get install -qq -y stress-ng python3-swiftclient "linux-tools-$(uname -r)"
-sudo modprobe intel_rapl_msr
-
-
